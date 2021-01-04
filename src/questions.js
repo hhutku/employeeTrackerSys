5e973cab3969df9d7b2d72b8364d18add325360a
hhutku <hhutku.wa@gmail.com>
2021-01-04 13:21:00 -0800

    questions.js added

---
+++ src/questions.js
@@ -0,0 +1,31 @@
+var SqlQueries = require("./sqlfile");
+
+
+var questions = {
+    type: "list",
+    name: "action",
+    message: "WHAT WOULD YOU LIKE TO MANAGE?",
+    choices: [
+        "VIEW ALL EMPLOYEES",
+        "VIEW DEPARTMENTS",
+        "VIEW ROLES",
+        "VIEW EMPLOYEES BY DEPARTMENT",
+        "VIEW EMPLOYEES BY ROLE",
+        "VIEW EMPLOYEES BY MANAGER",
+        "ADD EMPLOYEE",
+        "ADD DEPARTMENT",
+        "ADD ROLE",
+        "UPDATE EMPLOYEE ROLE",
+        "UPDATE EMPLOYEE MANAGER",
+        "DELETE DEPARTMENT",
+        "DELETE ROLE",
+        "REMOVE EMPLOYEE",
+        "QUIT"]
+
+};
+
+
+
+module.exports={questions,qUpdateManager,qUpdateRole,qAddEmployee,qAddRole
+
+}
